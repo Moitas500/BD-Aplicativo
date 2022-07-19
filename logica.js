fd4c2e16037b0606a9c197876310b8cfd724b7a9
Moitas500 <64492570+Moitas500@users.noreply.github.com>
2022-07-18 20:36:04 -0500

    Header
    Header front

---
+++ logica.js
@@ -0,0 +1,27 @@
+function agregarListener(){
+    var btnCrear = document.getElementById("crear");
+    btnCrear.addEventListener("click", function(){
+
+    })
+
+    var btnEditar = document.getElementById("editar");
+    btnEditar.addEventListener("click", function(){
+        
+    })
+
+    var btnBorrar = document.getElementById("borrar");
+    btnBorrar.addEventListener("click", function(){
+        
+    })
+
+    var btnActualizar = document.getElementById("actualizar");
+    btnActualizar.addEventListener("click", function(){
+        
+    })
+}
+
+function init(){
+    agregarListener();
+}
+
+init();
