f66257bb1f029fe4c19353b8e92637dc4fef70bc
Moitas500 <64492570+Moitas500@users.noreply.github.com>
2022-07-20 14:12:03 -0500

    Vista creada

--- logica.js
+++ logica.js
@@ -1,27 +1,83 @@
-function agregarListener(){
-    var btnCrear = document.getElementById("crear");
+var mostrado = false;  
+
+function añadirListener(){
+    var btnCrear = document.getElementById("crearBoton");
+
     btnCrear.addEventListener("click", function(){
+        if(!mostrado){
+            $("#crearContainer").show(); 
+            mostrado = true;
+        }else{
+            $("#actualizarContainer").hide();
+            $("#borrarContainer").hide();
 
-    })
+            $("#crearContainer").show(); 
+            mostrado = true;
+        }
+    });
 
-    var btnEditar = document.getElementById("editar");
+    var btnEditar = document.getElementById("editarBoton");
+
     btnEditar.addEventListener("click", function(){
-        
-    })
+        if(!mostrado){
+            $("#actualizarContainer").show(); 
+            mostrado = true;
+        }else{
+            $("#crearContainer").hide();
+            $("#borrarContainer").hide();
 
-    var btnBorrar = document.getElementById("borrar");
+            $("#actualizarContainer").show(); 
+            mostrado = true;
+        }
+    });
+
+    var btnBorrar = document.getElementById("borrarBoton");
+
     btnBorrar.addEventListener("click", function(){
-        
-    })
+        if(!mostrado){
+            $("#borrarContainer").show(); 
+            mostrado = true;
+        }else{
+            $("#actualizarContainer").hide();
+            $("#crearContainer").hide();
 
-    var btnActualizar = document.getElementById("actualizar");
-    btnActualizar.addEventListener("click", function(){
-        
-    })
+            $("#borrarContainer").show(); 
+            mostrado = true;
+        }
+    });
+
+    var btnEnviarCrear = document.getElementById("enviarCrear");
+
+    btnEnviarCrear.addEventListener("click", function(){
+        var rol = document.getElementById("rolCrear");
+        var sede = document.getElementById("sedeCrear");
+
+        if(rol.value == "0" || sede.value == "0"){
+            alert("Seleccione un rol y sede para el empleado para poder continuar");
+        }else{
+            //Aqui iria todo el codigo para crear empleado
+        }
+    });
+
+    var btnEnviarActualizar = document.getElementById("enviarActualizar");
+
+    btnEnviarActualizar.addEventListener("click", function(){
+        var rol = document.getElementById("rolActualizar");
+        var sede = document.getElementById("sedeActualizar");
+
+        if(rol.value == "0" || sede.value == "0"){
+            alert("Seleccione un rol y sede para el empleado para poder continuar");
+        }else{
+            //Aqui iria todo el codigo para crear empleado
+        }
+    });
 }
 
 function init(){
-    agregarListener();
+    añadirListener();
+    $("#crearContainer").hide();
+    $("#actualizarContainer").hide();
+    $("#borrarContainer").hide();
 }
 
 init();
