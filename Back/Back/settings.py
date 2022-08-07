c84adeda2d9f497c6787e98b618e05fed02585f4
Moitas500 <64492570+Moitas500@users.noreply.github.com>
2022-08-07 00:22:09 -0500

    Login funcionando

--- Back/Back/settings.py
+++ Back/Back/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
+ALLOWED_HOSTS=['*']
+CORS_ORIGIN_ALLOW_ALL = True
+
 ROOT_URLCONF = 'Back.urls'
 
 TEMPLATES = [
--- Front/logicaLogin.js
+++ Front/logicaLogin.js
@@ -1,14 +1,47 @@
+const API = "http://127.0.0.1:8000/empleado/";
+var empleados;
+
+$.ajax({
+    async: false,
+    url: "http://127.0.0.1:8000/empleadoCargo/",
+    type: 'GET',
+    dataType: 'json',
+    success: function (res) {
+        empleados = res
+    },
+})
+
 function añadirListener(){
     var btnLogin = document.getElementById("enviarLogin");
     btnLogin.addEventListener("click", function () {
         var inputCodigo = document.getElementById("codigo");
+
         if(inputCodigo.value == ""){
             alert("Por favor complete todos los campos");
         }else{
             //Validación del codigo del empleado
+            var encontrado = false;
+            var i = 0;
+
+            while(i < empleados.length){
+                if(empleados[i].codempleado.codempleado == inputCodigo.value){
+                    encontrado = true;
+                    break;
+                }else{
+                    i++;
+                }
+            }
+
+            if(encontrado){
                 //Diferenciar si es axuliar o director
-            document.location.href = "vistaAuxiliar.html";
-            //document.location.href = "vistaDirector.html";
+                if(empleados[i].idcargo.descargo == "Director Deportivo"){
+                    document.location.href = "vistaDirector.html";
+                }else{
+                    document.location.href = "vistaAuxiliar.html";
+                }
+            }else{
+                alert("El codigo digitado no existe para ningún empleado");
+            }
         }
     })
 }
