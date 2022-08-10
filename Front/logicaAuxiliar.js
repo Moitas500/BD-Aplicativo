8c3cbfa612a507e2b29569339855cb8427a7733f
Jose Quintero <jolquinteroc@correo.udistrital.edu.co>
2022-08-10 02:17:37 -0500

    front

--- Front/logicaAuxiliar.js
+++ Front/logicaAuxiliar.js
@@ -18,11 +18,11 @@ function Programaciones() {
     return respuesta;
 }
 
-function ResponsableEmpleado(IDempleado){
+function ResponsableEmpleado(IDempleado) {
     var respuesta
     $.ajax({
         async: false,
-        url: "http://127.0.0.1:8000/responsableDoc/"+IDempleado,
+        url: "http://127.0.0.1:8000/responsableDoc/" + IDempleado,
         type: 'GET',
         dataType: 'json',
         success: function (res) {
@@ -45,33 +45,33 @@ $.ajax({
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
+        results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function mostrarDatos(){
+function mostrarDatos() {
     var codigo = getParameterByName('codigo');
-    empleado = empleados[codigo-1];
+    empleado = empleados[codigo - 1];
 
     var today = new Date();
     var now = today.toLocaleString();
 
     var nombre = empleado.codempleado.nomempleado;
     var apellido = empleado.codempleado.apelempleado;
-    var sede =  empleado.codespacio.nomespacio;
+    var sede = empleado.codespacio.nomespacio;
 
     document.getElementById("fecha-hora").innerHTML = '<b>Fecha actual:</b> ' + now;
-    document.getElementById("nombreAuxiliar").innerHTML = '<b>Empleado:</b> ' + nombre + " " +apellido;
+    document.getElementById("nombreAuxiliar").innerHTML = '<b>Empleado:</b> ' + nombre + " " + apellido;
     document.getElementById("sedeAuxiliar").innerHTML = '<b>Sede:</b> ' + sede;
 }
 
-function añadirListener(){
+function añadirListener() {
     var btnAsistDocente = document.getElementById("asistDocenteBoton");
     btnAsistDocente.addEventListener("click", function () {
-        if(!mostrado){
+        if (!mostrado) {
             $("#docenteContainer").show();
             mostrado = true;
-        }else{
+        } else {
             $("#miembroEquipoContainer").hide();
             $("#estudianteContainer").hide();
             $("#tablaDatosDocente").hide();
@@ -85,10 +85,10 @@ function añadirListener(){
 
     var btnAsistPasante = document.getElementById("asistPasanteBoton");
     btnAsistPasante.addEventListener("click", function () {
-        if(!mostrado){
+        if (!mostrado) {
             $("#estudianteContainer").show();
             mostrado = true;
-        }else{
+        } else {
             $("#miembroEquipoContainer").hide();
             $("#docenteContainer").hide();
             $("#tablaDatosDocente").hide();
@@ -102,10 +102,10 @@ function añadirListener(){
 
     var btnAsistEquipo = document.getElementById("asistEquipoBoton");
     btnAsistEquipo.addEventListener("click", function () {
-        if(!mostrado){
+        if (!mostrado) {
             $("#miembroEquipoContainer").show();
             mostrado = true;
-        }else{
+        } else {
             $("#docenteContainer").hide();
             $("#estudianteContainer").hide();
             $("#tablaDatosDocente").hide();
@@ -116,33 +116,33 @@ function añadirListener(){
             mostrado = true;
         }
     })
-    
+
     var btnEnviarDocente = document.getElementById("enviarDocente");
     btnEnviarDocente.addEventListener("click", function () {
         var nombreDocente = document.getElementById("nombreDocente");
         var apellidoDocente = document.getElementById("apellidoDocente");
 
-        if(nombreDocente.value == "" || apellidoDocente.value == ""){
+        if (nombreDocente.value == "" || apellidoDocente.value == "") {
             alert("Verifique las casillas");
-        }else{
+        } else {
             var i = 0;
             var encontro = false;
-            while(i < empleados.length){
+            while (i < empleados.length) {
                 var empleado = empleados[i];
 
-                if(nombreDocente.value.toLowerCase() == empleado.codempleado.nomempleado.toLowerCase()  && apellidoDocente.value.toLowerCase()  == empleado.codempleado.apelempleado.toLowerCase() ){
+                if (nombreDocente.value.toLowerCase() == empleado.codempleado.nomempleado.toLowerCase() && apellidoDocente.value.toLowerCase() == empleado.codempleado.apelempleado.toLowerCase()) {
                     encontro = true;
-                    if(empleado.idcargo.descargo == "Docente"){ 
+                    if (empleado.idcargo.descargo == "Docente") {
                         asistenteDocente(empleado);
                         break
-                    }else{
+                    } else {
                         alert("El nombre y apellido digitado no corresponde a un docente");
                         break;
                     }
                 }
                 i++;
             }
-            if (!encontro){
+            if (!encontro) {
                 alert("El nombre y apellido digitado no corresponde a un empleado");
             }
         }
@@ -152,9 +152,9 @@ function añadirListener(){
     btnEnviarPasante.addEventListener("click", function () {
         var codigoEstudiante = document.getElementById("nombreEstudiante");
 
-        if(codigoEstudiante.value == ""){
+        if (codigoEstudiante.value == "") {
             alert("Verifique las casillas");
-        }else{
+        } else {
             var codigo = document.getElementById("nombreEstudiante");
             var estudiante;
 
@@ -168,11 +168,11 @@ function añadirListener(){
                 },
             })
 
-            if(codigoEstudiante.value == estudiante.codestudiante){
+            if (codigoEstudiante.value == estudiante.codestudiante) {
                 asistenciaPasante(estudiante);
-            }else{
+            } else {
                 alert("El codigo digitado no pertenece a ningún estudiante");
-            }   
+            }
         }
     })
 
@@ -181,9 +181,9 @@ function añadirListener(){
         var codigoEstudiante = document.getElementById("codigoEstudiante");
         var codigoEquipo = document.getElementById("nombreEquipo");
 
-        if(codigoEstudiante.value == "" || codigoEquipo.value == ""){
+        if (codigoEstudiante.value == "" || codigoEquipo.value == "") {
             alert("Verifique las casillas");
-        }else{
+        } else {
             var miembroEquipo;
             var estudiante;
             var equipos;
@@ -208,9 +208,9 @@ function añadirListener(){
                 },
             })
 
-            if(estudiante.codestudiante == "" || codigoEquipo > equipos.length){
+            if (estudiante.codestudiante == "" || codigoEquipo > equipos.length) {
                 alert("Los datos son invalidos");
-            }else{
+            } else {
                 var equipo = equipos[codigoEquipo.value];
                 var miembroEquipo;
 
@@ -224,9 +224,9 @@ function añadirListener(){
                     },
                 })
 
-                if(miembroEquipo.length == 0){
+                if (miembroEquipo.length == 0) {
                     alert("El estudiante no pertenece al equipo digitado");
-                }else{
+                } else {
                     asistenciaMiembroEquipo(miembroEquipo);
                 }
             }
@@ -234,14 +234,14 @@ function añadirListener(){
     })
 }
 
-function asistenciaMiembroEquipo(miembroEquipo){
+function asistenciaMiembroEquipo(miembroEquipo) {
     var nombreEquipo = miembroEquipo[0].codestudiante.nomestudiante;
     var apellidoEquipo = miembroEquipo[0].codestudiante.apelestudiante;
     var sedeEquipo = miembroEquipo[0].codestudiante.codespacio.nomespacio;
     var today = new Date();
     var now = today.toLocaleString();
     var programacion;
-    var hora  = now.split(',');
+    var hora = now.split(',');
     hora = hora[1].split(' ');
     hora = hora[1].split(':');
     var i = 0;
@@ -261,29 +261,29 @@ function asistenciaMiembroEquipo(miembroEquipo){
         },
     })
 
-    while(i < programacion.length){
+    while (i < programacion.length) {
         var prog = programacion[i];
 
-        if(prog.iddeporte.iddeporte == miembroEquipo[0].conseequipo.iddeporte.iddeporte){
+        if (prog.iddeporte.iddeporte == miembroEquipo[0].conseequipo.iddeporte.iddeporte) {
             var horaIni = prog.idhora_ini;
             var horaFin = prog.idhora_fin;
             horaIni = horaIni.split(':');
             horaFin = horaFin.split(':');
 
-            if((parseInt(horaIni[0])-1) == parseInt(hora[0])){
+            if ((parseInt(horaIni[0]) - 1) == parseInt(hora[0])) {
                 var diferencia = 60 - parseInt(hora[1]);
 
-                if(diferencia <= 15){
+                if (diferencia <= 15) {
                     registrarAsistenciaMiembroEquipo(miembroEquipo, prog);
-                }else{
+                } else {
                     alert("No se puede registrar la asistencia");
                 }
-            }else if(parseInt(horaIni[0]) == 6){
+            } else if (parseInt(horaIni[0]) == 9) {
                 var diferencia = 60 - parseInt(hora[1]);
 
-                if(diferencia >=45){
+                if (diferencia >= 45) {
                     registrarAsistenciaMiembroEquipo(miembroEquipo, prog);
-                }else{
+                } else {
                     alert("No se puede registrar la asistencia");
                 }
             }
@@ -297,35 +297,53 @@ function asistenciaMiembroEquipo(miembroEquipo){
     $("#tablaDatosEquipo").show();
 }
 
-function registrarAsistenciaMiembroEquipo(miembroEquipo, programacion){
+function registrarAsistenciaMiembroEquipo(miembroEquipo, programacion) {
     $.ajax({
         async: false,
-        url: "http://localhost:8081/asistenciaMiembro/",
+        url: "http://localhost:8000/asistenciaMiembro/",
         type: 'POST',
         dataType: 'json',
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify({
             "consecprogramacion": programacion.consecprogramacion,
-            "conmiembroequipo": 1,
+            "conmiembroequipo": programacion.consecprogramacion + 1,
             "conseequipo": miembroEquipo[0].conseequipo.conseequipo,
             "itemmiembro": miembroEquipo[0].itemmiembro,
         }),
         success: function () {
-            console.log("enviado")
+            alert("Asistencia registrada con exito")
         },
     })
 }
 
-function asistenteDocente(empleado){
-    var nombreDocente  = empleado.codempleado.nomempleado;
+function registrarAsistenciaDocente(responable) {
+    $.ajax({
+        async: false,
+        url: "http://localhost:8000/asistenciaResponsable/",
+        type: 'POST',
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({
+            "consecprogramacion": responable.consecprogramacion.consecprogramacion,
+            "concecres": responable.concecres,
+            "consecasisres": responable.consecprogramacion.consecprogramacion,
+        }),
+        success: function () {
+            alert("Asistencia registrada con exito")
+        },
+    })
+}
+
+function asistenteDocente(empleado) {
+    var nombreDocente = empleado.codempleado.nomempleado;
     var apellidoDocente = empleado.codempleado.apelempleado;
     var sedeDocente = empleado.codespacio.nomespacio;
     var today = new Date();
     var now = today.toLocaleString();
-    var programacion = Programaciones();
-    var hora  = now.split(',');
-    var responsable;
+    var hora = now.split(',');
     hora = hora[1].split(' ');
+    hora = hora[1].split(':');
+    var responsable;
 
     document.getElementById("thNombreDocente").textContent = nombreDocente;
     document.getElementById("thApellidoDocente").textContent = apellidoDocente;
@@ -333,42 +351,38 @@ function asistenteDocente(empleado){
     document.getElementById("thFecha").textContent = now;
 
     responsable = ResponsableEmpleado(empleado.codempleado.codempleado)
-
+    bandera = false;
     responsable.forEach(res => {
+        debugger
         console.log(res)
         var horaIni = res.consecprogramacion.idhora_ini;
         var horaFin = res.consecprogramacion.idhora_fin;
+        horaIni = horaIni.split(':');
+        horaFin = horaFin.split(':');
 
-        if((parseInt(horaIni[0])) == parseInt(hora[0])){
-            var diferencia = 60 - parseInt(hora[1]);
-
-            if(diferencia <= 15){
-                //Aqui devuelve todo
-            }else{
-                alert("No se puede realizar el procedimiento");
-            }
-        }else if(parseInt(horaIni[0]) == 9){
-            var diferencia = 60 - parseInt(hora[1]);
-
-            if(diferencia >=45){
-                //Aqui devuelve todo
-            }else{
-                alert("No se puede realizar el procedimiento");
-            }
+        // if ((parseInt(horaIni[0])) <= parseInt(hora[0]) && (parseInt(horaFin[0])) >= parseInt(hora[0])) {
+        if ((parseInt(horaIni[0])) <= 11 && (parseInt(horaFin[0])) >= 11) {
+            registrarAsistenciaDocente(res)
+            bandera = true
+            $("#tablaDatosDocente").show();
+            // Comentariar
+            
         }
     });
 
-    $("#tablaDatosDocente").show();
+    if (!bandera) {
+        alert("No se puede realizar el procedimiento");
+    }
 }
 
-function asistenciaPasante(estudiante){
+function asistenciaPasante(estudiante) {
     var nombre = estudiante.nomestudiante;
     var apellido = estudiante.apelestudiante;
     var sede = estudiante.codespacio.nomespacio;
     var today = new Date();
     var now = today.toLocaleString();
     var programacion;
-    var hora  = now.split(',');
+    var hora = now.split(',');
     var responsable;
     hora = hora[1].split(' ');
 
@@ -394,20 +408,20 @@ function asistenciaPasante(estudiante){
         var horaIni = res.consecprogramacion.idhora_ini;
         var horaFin = res.consecprogramacion.idhora_fin;
 
-        if((parseInt(horaIni[0])) == parseInt(hora[0])){
+        if ((parseInt(horaIni[0])) == parseInt(hora[0])) {
             var diferencia = 60 - parseInt(hora[1]);
 
-            if(diferencia <= 15){
+            if (diferencia <= 15) {
                 //Aqui devuelve todo
-            }else{
+            } else {
                 alert("No se puede realizar el procedimiento");
             }
-        }else if(parseInt(horaIni[0]) == 9){
+        } else if (parseInt(horaIni[0]) == 9) {
             var diferencia = 60 - parseInt(hora[1]);
 
-            if(diferencia >=45){
+            if (diferencia >= 45) {
                 //Aqui devuelve todo
-            }else{
+            } else {
                 alert("No se puede realizar el procedimiento");
             }
         }
@@ -416,7 +430,7 @@ function asistenciaPasante(estudiante){
     $("#tablaDatosEstudiante").show();
 }
 
-function init(){
+function init() {
     añadirListener();
     mostrarDatos();
     $("#docenteContainer").hide();
